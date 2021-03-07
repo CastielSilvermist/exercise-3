3f365247a8f7daea0c3cf0b7279a3c1cc8dab708
Jorinda Becker <jorindabecker@gmail.com>
2021-03-07 11:31:48 -0700

    working quote gallery. ready for image conversion

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,58 @@
-// var quote = [
-// '"We don\'t make mistakes, just happy little accidents." - Bob Ross"',
-// '"Creativity takes courage" - Henry Matisse'
-// ]
+function Quote(quoteText, author, tags, color) {
+	this.quote = quoteText;
+	this.author = author;
+	this.tags = tags;
+	this.color = color;
+	this.display = function() {
 
-var quote1 = {
-	quote: '"We don\'t make mistakes, just happy little accidents."',
-	author: "Bob Ross",
-	tags: ["painting", "mistakes"],
-	color: "#0a3410",
-	display: function() {
+		var container = $("<div>")
+		this.tags.forEach(function(tag){
+			container.addClass(tag);
+		})
+		container.css("background", this.color)
+		container.addClass("quote")
+
 		var quoteString = "";
-		quoteString += "<div style='background:" + this.color + "'>";
+		//use image tag instead of paragraph
 		quoteString += "<p>" + this.quote + "</p>";
 		quoteString += "<cite>" + this.author + "</cite>";
-		quoteString += "</div>"
-		$("body").prepend(quoteString)
+	
+		container.html(quoteString)
+		$(".quotes").prepend(container)
 	}
 }
+//imgs use URL. so how to add url to this
+var quotes = [
+	new Quote('"We don\'t make mistakes, just happy little accidents."',"Bob Ross",["painting", "mistakes"],"#0a3410"),
+	new Quote('"Creativity takes courage"',"- Henry Matisse", ["painting", "creativity"], "lightblue")
+]
 
-quote1.display();
+//global taglist
+var tagList = []
 
+quotes.forEach(function(quote){
+	quote.display();
+	quote.tags.forEach(function(tag){
+		//check to see if the tag has been added to the taglist
+		if(!tagList.includes(tag)){
+			//if it isn't in the list, add it
+			tagList.push(tag);
+			//and make a button for it too.
+			$(".buttons").prepend("<button class='filter'>" + tag + "</button>")
+		}	
+	})
+});
 
+console.log(tagList)
 
-console.log(quote1.quote)
+$(".filter").on("click", function(){
+	var tag = $(this).text();
+	console.log(tag)
 
-// var quoteString = "";
-// quote.forEach(function(quote){
-	
+	$("." + tag).fadeIn();
+	$(".quote").not("." + tag).hide();
 
-// 	quoteString += "<p>" + quote + "</p>"
-
-// 	console.log(quoteString);
-
-	
-// })
-
-// $("body").html(quoteString)
+//remove class active resets the buttons to not show the styling for active button
+	$(".filter").removeClass("active")
+	$(this).addClass("active")
+})
